a3ef5170238e995e2e37c7f39fd78c4711a0e2c9
Celso David Lopes dos Santos <celunico43@gmail.com>
2019-08-21 13:29:38 -0300

    Aplicando o principio de Responsabilidade Unica, criei uma nova classe responsavel por formartar o resultado

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ use CESANTOS\PHPSOLID\SRP\Report;
 use CESANTOS\PHPSOLID\SRP\JsonReportFormatter;
 
 # Single responsibility principle (Princípio da responsabilidade única)
+
 $report = new JsonReportFormatter();
 var_dump($report->format(new Report()));
