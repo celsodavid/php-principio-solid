b2512f0f8462dd34389b5c455a6820c34c96d45a
Celso David Lopes dos Santos <celunico43@gmail.com>
2019-08-21 13:33:13 -0300

    Neste exemplo a classe Logger possui dois metodos que geram tipos de arquivos diferentes, caso precise criar mais um tipo, teremos que mexer na classe Logger, isso fere com o principio de aberto/fechado

---
+++ src/OCP/Logger.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace CESANTOS\PHPSOLID\OCP;
+
+class Logger
+{
+    public function writeTxt($message)
+    {
+        // logica
+    }
+
+    public function writeCsv($message)
+    {
+        // logica
+    }
+}
